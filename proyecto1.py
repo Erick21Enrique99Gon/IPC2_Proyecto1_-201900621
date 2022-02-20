bba0fd4361eed659c38ca1ce545713b0bf0f82db
Erick21Enrique99Gon <61717279+Erick21Enrique99Gon@users.noreply.github.com>
2022-02-20 17:38:10 -0600

    segunda semana

--- proyecto1.py
+++ proyecto1.py
@@ -1,6 +1,27 @@
-import easygui
+'''import easygui
+from xml.dom.minidom import parse
 
-r = easygui.fileopenbox()
-archivo = open(r,'r',encoding="utf8")
-print(archivo.read())
-archivo.close()
+#r = easygui.fileopenbox()
+#archivo = open(r,'r',encoding="utf8")
+#print(archivo.read())
+a = minidom.parse("datos.xml")
+#archivo.close()
+
+#if __name__ == '__main__':
+#    print("main")'''
+
+from xml.dom.minidom import parse 
+
+doc = parse("datos.xml")
+
+# doc.getElementsByTagName returns NodeList
+name = doc.getElementsByTagName("name")[0]
+print(name.firstChild.data)
+
+staffs = doc.getElementsByTagName("staff")
+for staff in staffs:
+        sid = staff.getAttribute("id")
+        nickname = staff.getElementsByTagName("nickname")[0]
+        salary = staff.getElementsByTagName("salary")[0]
+        print("id:%s, nickname:%s, salary:%s" %
+              (sid, nickname.firstChild.data, salary.firstChild.data))
